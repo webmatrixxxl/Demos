795051a4f633e6fb2bfd1dc6a59c12a8bd2f0776
Кирил Даскалов <webmatrix@mail.bg>
2013-04-18 13:07:05 +0300

    SignalR Live Chat

---
+++ SignalR Chat/SignalR Chat/MessagesHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+
+namespace SignalR_Chat
+{
+    [HubName("MessagesHub")]
+    public class MessagesHub : Hub
+    {
+        public void SendMessages(string message, string color)
+        {
+            //Call displayMessages on client
+            Clients.All.displayMessages(message, color);
+        }
+    }
+}
---
+++ SignalR Chat/SignalR Chat/MyChat.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="Scripts/jquery-1.6.4.js" />
+/// <reference path="Scripts/jquery.signalR-1.0.1.js" />
+$(function () {
+    var chat = $.connection.MessagesHub;
+
+    var randColor1 = Math.floor((Math.random() * 150) + 100),
+        randColor2 = Math.floor((Math.random() * 150) + 100),
+        randColor3 = Math.floor((Math.random() * 150) + 100)
+
+    var color = "rgb(" + randColor1 + "," + randColor2 + "," + randColor3 + ")";
+    console.log(color);
+
+    chat.client.displayMessages = function (message, color) {
+        $("#messagesList").prepend("<li style=background-color:" + color + ">" + message + "</li>");
+    }
+
+    $.connection.hub.start().done(function () {
+        //On Send button click calls SendMessages() from MessagesHub.cs
+        $("#send").click(function () {
+            chat.server.sendMessages($("#message").val(), color);
+        })
+    })
+});
